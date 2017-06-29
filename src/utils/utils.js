a967d2ce01091fc535c592791e70a51e277283af
ArmandoAssuncao <armando.assuncao.93@gmail.com>
2017-06-29 01:26:57 -0300

    added method for get location in utils

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,4 +1,5 @@
 import { NavigationActions } from 'react-navigation';
+import { Alert, PermissionsAndroid } from 'react-native';
 
 export const resetNavigateTo = (routeName: string, navigation) => {
   const actionToDispatch = NavigationActions.reset({
@@ -6,4 +7,73 @@ export const resetNavigateTo = (routeName: string, navigation) => {
     actions: [NavigationActions.navigate({ routeName })]
   });
   navigation.dispatch(actionToDispatch);
+};
+
+export const requestPermissions = () => {
+  PermissionsAndroid.requestMultiple([
+    PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+  ])
+  .then(permissions => {
+    let showMsg = false;
+    let msgs = [];
+
+    if(permissions['android.permission.ACCESS_FINE_LOCATION'] === PermissionsAndroid.RESULTS.DENIED) {
+      msgs.push('Allow the location for you can talk with others people.');
+      showMsg = true;
+    }
+
+    if(showMsg === true) {
+      showAlertWarning(msgs.join('\n'), 'Permissions', 'Close');
+    }
+  })
+  .catch(error => {
+    console.error('request permissions: ', error);
+  });
+};
+
+export const getLocation = (navigator, timeout=315360000, highAccuracy=false, maxAge=3600000) => {
+  return new Promise(function(resolve, reject){
+    // check location permission
+    PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION)
+    .then(granted => {
+      if(granted === false) {
+        reject(new Error('permission location denied'));
+      }
+      else {
+        // resolve coordinates
+        navigator.geolocation.getCurrentPosition(
+          position => {
+            const coords = {
+              lon: position.coords.longitude,
+              lat: position.coords.latitude,
+            };
+            resolve(coords);
+          },
+          error => {
+            reject(error);
+          },
+          {enableHighAccuracy: highAccuracy, timeout: timeout, maximumAge: maxAge}
+        );
+      }
+    })
+    .catch(error => {
+      reject(error);
+    });
+  });
+};
+
+export const showAlertWarning = (msg='Could not perform the action', title='Alert', btnText='OK', btnPress = () => {}) => {
+  Alert.alert(
+    title,
+    msg,
+    [
+      {text: btnText, onPress: btnPress},
+    ],
+  );
+};
+
+export const generateRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
 };
